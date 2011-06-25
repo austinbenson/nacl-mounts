74b0034658d3bdee44894f7f8280258877ca8856
Ivan Krasin <krasin@google.com>
2011-06-26 07:33:46 +0800

    Remove FileHandleTest from AllTest.cc. Now all tests GREEN

--- tests/common/AllTest.cc
+++ tests/common/AllTest.cc
@@ -1,6 +1,5 @@
 #include "../base/MountManagerTest.cc"
 #include "../base/NodeTest.cc"
-#include "../base/FileHandleTest.cc"
 #include "../base/PathHandleTest.cc"
 #include "../memory/MemFileHandleTest.cc"
 #include "../memory/MemNodeTest.cc"
